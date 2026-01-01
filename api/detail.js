8eb91bc4eed397d3ed0d27e1cb21e5d3b6353947
kalz <chnationg@gmail.com>
2026-01-01 22:54:36 +0200

    Update detail.js

--- api/detail.js
+++ api/detail.js
@@ -1,20 +1,14 @@
-
 export default async function handler(req, res) {
   const { urlId } = req.query;
 
   if (!urlId) {
-    return res.status(400).json({ error: "urlId required" });
+    return res.status(400).json({ error: "urlId dibutuhkan" });
   }
 
-  try {
-    const r = await fetch(
-      "https://api.sansekai.my.id/api/anime/detail?urlId=" + urlId
-    );
-    const data = await r.json();
+  const r = await fetch(
+    `https://api.sansekai.my.id/api/anime/detail?urlId=${urlId}`
+  );
+  const data = await r.json();
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json(data);
-  } catch (e) {
-    res.status(500).json({ error: "failed fetch detail" });
-  }
+  res.status(200).json(data);
 }
