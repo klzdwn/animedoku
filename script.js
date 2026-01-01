15f7f4143566123845ac455665c0a00851e4b369
kalz <chnationg@gmail.com>
2026-01-01 22:11:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-
-const API = "https://animedoku.vercel.app/api/latest";
+const API = "https://api.sansekai.my.id/api/anime/latest";
 
 const status = document.getElementById("status");
 const list = document.getElementById("list");
 
 fetch(API)
   .then(res => res.json())
-  .then(data => {
+  .then(json => {
+    const data = json.data || json; // jaga-jaga
+
     if (!Array.isArray(data)) {
       status.textContent = "Data API tidak valid";
       return;
@@ -16,14 +17,14 @@ fetch(API)
     list.innerHTML = "";
 
     data.forEach(anime => {
-      if (!anime.slug) return;
+      if (!anime.url) return;
 
       const a = document.createElement("a");
-      a.href = `detail.html?slug=${anime.slug}`;
+      a.href = `detail.html?url=${encodeURIComponent(anime.url)}`;
       a.textContent = anime.judul;
       a.style.display = "block";
-      a.style.padding = "6px 0";
       a.style.color = "white";
+      a.style.padding = "6px 0";
       a.style.textDecoration = "none";
 
       list.appendChild(a);
